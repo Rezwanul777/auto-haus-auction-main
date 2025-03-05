0d88ce191e966abf33da70eeacf47f8f6b35907c
Rezwanul777 <rezwanul.haquebd777@gmail.com>
2025-03-06 00:00:11 +0600

    Utvalda bilar responsive and home button fixed

--- src/components/header/SearchBar.tsx
+++ src/components/header/SearchBar.tsx
@@ -3,7 +3,7 @@ import { Input } from "../ui/input";
 
 const SearchBar = () => {
   return (
-    <div className="w-[200px]">
+    <div className="lg:w-[200px] sm:w-[140px]">
       <div className="relative">
         <div className="absolute inset-y-0 left-0 pl-2 flex items-center pointer-events-none">
           <Search className="h-3 w-3 text-gray-400" />
@@ -11,7 +11,7 @@ const SearchBar = () => {
         <Input
           type="search"
           placeholder="Sök efter bilar..."
-          className="w-full pl-7 py-1 text-xs bg-transparent border-gray-200/20 focus:bg-white/5 transition-colors h-7"
+          className="lg:w-full sm:w-1/2 pl-7 py-1 text-xs bg-transparent border-gray-200/20 focus:bg-white/5 transition-colors h-7"
         />
       </div>
     </div>
